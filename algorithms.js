526344ff1d33137ae0d665890596d606e84bfa6d
Toby Jorris <toby.jorris@appealmaker.com>
2022-01-14 09:38:18 -0600

    push algorithms.js

---
+++ algorithms.js
@@ -0,0 +1,16 @@
+//Hamming Distance - return a numerical 'distance' between two strings
+const hammingDistance = (string1, string2) => {
+    const string1Array = string1.split('')
+    const string2Array = string2.split('')
+    let count = 0
+
+    string1Array.forEach((character, index) => {
+        if (character !== string2Array[index]) {
+            count++
+        }
+    })
+
+    return count
+}
+
+hammingDistance('karolin', 'kathrin')
