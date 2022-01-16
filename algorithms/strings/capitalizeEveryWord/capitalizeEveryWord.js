0d007b5f657c79addc5af971937ee604ef2278ee
Toby Jorris <toby.jorris@appealmaker.com>
2022-01-16 12:24:55 -0600

    added String prototype extension

--- algorithms/strings/capitalizeEveryWord/capitalizeEveryWord.js
+++ algorithms/strings/capitalizeEveryWord/capitalizeEveryWord.js
@@ -2,4 +2,11 @@
 
 const capitalizeEveryWord = string => string.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
 
+//Variation - could extend the String prototype like this:
+
+// String.prototype.capitalizeEveryWord = function () {
+//   return this.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+// };
+// 'this is a test'.capitalizeEveryWord()
+
 module.exports = capitalizeEveryWord
